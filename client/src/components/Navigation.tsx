8261266d4a7df879799b037832cfac258e30a95d
03gibbss <sam-gibbs1@hotmail.co.uk>
2023-09-16 15:00:04 +0100

    task 2: fixed wallet connection by adding injectedModule to wallets

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,10 +1,13 @@
 import React, { useCallback, useState } from 'react';
-import Onboard, { WalletState } from '@web3-onboard/core'
+import Onboard, { WalletState } from '@web3-onboard/core';
+import injectedModule from '@web3-onboard/injected-wallets';
 
 import SendTransaction from './SendTransaction';
 
+const injected = injectedModule();
+
 const onboard = Onboard({
-  wallets: [],
+  wallets: [injected],
   chains: [
     {
       id: '123456',
@@ -12,8 +15,8 @@ const onboard = Onboard({
       label: 'Local Ganache',
       rpcUrl: 'http://localhost:8545',
     },
-  ]
-})
+  ],
+});
 
 const Navigation: React.FC = () => {
   const [wallet, setWallet] = useState<WalletState>();
@@ -32,7 +35,9 @@ const Navigation: React.FC = () => {
     <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-ful text-sm py-4 bg-gray-800">
       <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
         <div className="flex items-center justify-between">
-          <a className="flex-none text-xl font-semibold dark:text-white" href=".">Transactions List</a>
+          <a className="flex-none text-xl font-semibold dark:text-white" href=".">
+            Transactions List
+          </a>
         </div>
         <div className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
           <div className="flex flex-col gap-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
@@ -44,14 +49,19 @@ const Navigation: React.FC = () => {
                 </p>
               </>
             )}
-            {!wallet && <button type="button" onClick={handleConnect} className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-gray-200 hover:text-white hover:bg-gray-500 hover:border-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-offset-2 transition-all text-sm">
-              Connect Wallet
-            </button>}
+            {!wallet && (
+              <button
+                type="button"
+                onClick={handleConnect}
+                className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-gray-200 hover:text-white hover:bg-gray-500 hover:border-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-offset-2 transition-all text-sm">
+                Connect Wallet
+              </button>
+            )}
           </div>
         </div>
       </nav>
     </header>
   );
-}
+};
 
 export default Navigation;
